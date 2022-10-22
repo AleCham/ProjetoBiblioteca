d7f09829b320fd5f94fa9011b62827863f54784e
AleCham <alessandra.cham@hotmail.com>
2022-10-22 01:42:14 -0300

    Terceiro ajuste aplicado

--- Biblioteca/Models/EmprestimoService.cs
+++ Biblioteca/Models/EmprestimoService.cs
@@ -31,13 +31,53 @@ namespace Biblioteca.Models
             }
         }
 
-        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
+        // public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
+        // {
+        //     using(BibliotecaContext bc = new BibliotecaContext())
+        //     {
+        //         return bc.Emprestimos.Include(e => e.Livro).ToList();
+        //     }
+        // }
+
+        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro = null)
         {
             using(BibliotecaContext bc = new BibliotecaContext())
             {
-                return bc.Emprestimos.Include(e => e.Livro).ToList();
+                IQueryable<Emprestimo> query;
+                
+                if(filtro != null)
+                {
+                    //definindo dinamicamente a filtragem
+                    switch(filtro.TipoFiltro)
+                    {
+                        case "Usuario":
+                            query = bc.Emprestimos.Where(e => e.NomeUsuario.Contains(filtro.Filtro.ToLower()));
+                        break;
+
+                        case "Livro":
+                            query = bc.Emprestimos.Where(e => e.Livro.Titulo.Contains(filtro.Filtro.ToLower()));
+                        break;
+
+                        default:
+                            query = bc.Emprestimos;
+                        break;
+                    }
+                    
+                }
+                else
+                {
+                    // caso filtro não tenha sido informado
+                    query = bc.Emprestimos;
+                }
+                
+                //ordenação padrão
+                return query.Include(e => e.Livro).OrderByDescending(e => e.DataDevolucao).ToList();
             }
         }
+
+
+
+
 
         public Emprestimo ObterPorId(int id)
         {
