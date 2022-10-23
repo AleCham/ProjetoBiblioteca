c89d5c4ceb2296d779bb156ae3e5c605a8788725
AleCham <alessandra.cham@hotmail.com>
2022-10-23 03:21:45 -0300

    Quarto ajuste aplicado

---
+++ Biblioteca/Models/UsuarioService.cs
@@ -0,0 +1,69 @@
+using System.ComponentModel;
+using System.Reflection.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Models
+{
+    public class UsuarioService
+    {
+        public List<Usuario> Listar()
+        {
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
+                return bc.Usuarios.ToList();
+            }
+
+        }
+
+        public void incluirUsuario(Usuario user)
+        {
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
+                bc.Usuarios.Add(user);
+
+                bc.SaveChanges();
+            }
+
+        }
+
+        public void editarUsuario(Usuario userEditado)
+        {
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
+                Usuario u = bc.Usuarios.Find(userEditado.Id);
+
+                u.Nome = userEditado.Nome;
+                u.Login = userEditado.Login;
+                u.Senha = userEditado.Senha;
+                u.Tipo = userEditado.Tipo;
+
+                bc.SaveChanges();
+            }
+        }
+
+        public void excluirUsuario(int id)
+        {
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
+                bc.Usuarios.Remove(bc.Usuarios.Find(id));
+
+                bc.SaveChanges();
+            }
+
+        }
+
+        public Usuario buscarPorId(int id)
+        {
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
+                return bc.Usuarios.Find(id);
+            }
+
+
+
+        }
+    }
+}
